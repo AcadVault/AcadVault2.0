09479edfd7a5e4d7ffcd88ae4497d91813fd8dd9
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-01 01:00:59 +0530

    Added NavBar and updated Profile page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/components/sessionProvider";
+import NavBar from "../components/NavBar"
 
 export const metadata = {
   title: "AcadVault 2.0",
@@ -15,13 +16,16 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div class="fixed left-0 top-0 -z-10 h-full w-full">
-        <div class="relative h-full w-full bg-slate-950">
-          <div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-          <div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div class="fixed left-0 top-0 -z-10 h-full w-full">
+          <div class="relative h-full w-full bg-slate-950">
+            <div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+            <div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          </div>
         </div>
-      </div>
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <NavBar />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
