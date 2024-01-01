cea7d16c86fa64a018d8a94709d25b9fb7b28a56
Jam-Burger <jay40793@gmail.com>
2024-01-01 12:27:26 +0530

    solve deploy issues-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,32 +2,29 @@
 
 import { useSession } from "next-auth/react";
 
-const Navbar = () => {
+const NavBar = () => {
   const session = useSession();
 
   if (session.status === "unauthenticated") {
     return (
-      <nav className="bg-transparent">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <a
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+      <nav class="bg-transparent">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               AcadVault2.0
             </span>
           </a>
-          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+          <div class="flex items-center space-x-6 rtl:space-x-reverse">
             <a
               href="https://github.com/AcadVault/AcadVault2.0"
               target="_blank"
-              className="text-sm  text-gray-500 dark:text-white hover:underline"
+              class="text-sm  text-gray-500 dark:text-white hover:underline"
             >
               Github
             </a>
             <a
               href="/login"
-              className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+              class="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
             >
               SignIn
             </a>
@@ -37,21 +34,18 @@ const Navbar = () => {
     );
   } else {
     return (
-      <nav className="bg-transparent">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <a
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+      <nav class="bg-transparent">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               AcadVault2.0
             </span>
           </a>
-          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+          <div class="flex items-center space-x-6 rtl:space-x-reverse">
             <a
               href="https://github.com/AcadVault/AcadVault2.0"
               target="_blank"
-              className="text-sm  text-gray-500 dark:text-white hover:underline"
+              class="text-sm  text-gray-500 dark:text-white hover:underline"
             >
               Github
             </a>
@@ -62,4 +56,4 @@ const Navbar = () => {
   }
 };
 
-export default Navbar;
+export default NavBar;
