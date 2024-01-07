3e5726019b7d8e19baf74725904e03501631db9a
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-07 15:21:26 +0530

    Update grid layout and card text styles

--- src/app/requests/page.jsx
+++ src/app/requests/page.jsx
@@ -22,7 +22,7 @@ function RequestsPage() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 w-3/4 mx-auto gap-5 lg:grid-cols-2 mt-10">
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 w-11/12 sm:w-4/5 md:w-3/4 mx-auto my-10">
       {data.map((request, index) => (
         <RequestCard data={request} currentUser={session.data.user} key={index} />
       ))}
--- src/components/BrowseCard.jsx
+++ src/components/BrowseCard.jsx
@@ -7,7 +7,7 @@ const BrowseCard = ({ children, href, isSquare = true }) => {
   return (
     <a href={href} className={font.className}>
       <div className={isSquare ? "aspect-square" : ""}>
-        <div className="flex items-center justify-center w-full h-full text-center bg-white bg-opacity-5 rounded-lg p-7 text-secondary hover:bg-opacity-10 hover:scale-105 transition ease-linear cursor-pointer">
+        <div className="flex items-center justify-center w-full h-full text-center bg-white bg-opacity-5 rounded-lg p-4 text-secondary hover:bg-opacity-10 hover:scale-105 transition ease-linear cursor-pointer">
           {children}
         </div>
       </div>
