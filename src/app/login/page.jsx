51ab3e1f9198f8067a044a1f5cf676c6e8c87e9e
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-08 21:34:16 +0530

    Update Navbar component and remove About button

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -30,13 +30,6 @@ const LoginPage = () => {
               <FcGoogle className="w-4 h-4 me-2" /> Login with Google
             </span>
           </button>
-          <a href="/about">
-            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-              <span className="relative inline-flex items-center px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                About
-              </span>
-            </button>
-          </a>
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,136 +1,181 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import { usePathname } from "next/navigation";
-import { FaRegUserCircle } from "react-icons/fa";
+import { useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { FaRegUserCircle } from 'react-icons/fa'
 
-const NavBar = ({ isResourceManager }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const pathname = usePathname();
-  const activeColor = "#D791FF";
+const NavBar = ({ isResourceManager, session }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const pathname = usePathname()
+    const activeColor = '#D791FF'
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
 
-  return (
-    <nav className="bg-white bg-opacity-[0.02]">
-      <div
-        className={`${
-          isMenuOpen ? "bg-black bg-opacity-20" : ""
-        } flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4 transition-all duration-300`}
-      >
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            AcadVault2.0
-          </span>
-        </a>
-        <div className={"flex flex-col rtl:space-x-reverse"}>
-          <button
-            className="text-white focus:outline-none lg:hidden"
-            onClick={toggleMenu}
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+    return (
+        <nav className="bg-white bg-opacity-[0.02] backdrop backdrop-blur-sm">
+            <div
+                className={`${
+                    isMenuOpen ? 'bg-black bg-opacity-20' : ''
+                } flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4 transition-all duration-300`}
             >
-              {isMenuOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              )}
-            </svg>
-          </button>
-          <div
-            className={`${
-              isMenuOpen
-                ? "flex absolute right-5 top-16 py-3 z-50 bg-white bg-opacity-90 rounded-md  min-w-[200px] items-center"
-                : "hidden"
-            } flex-col lg:flex lg:flex-row lg:items-center lg:w-auto lg:space-x-6 rtl:space-x-reverse`}
-          >
-            {pathname !== "/login" && (
-              <>
                 <a
-                  href="/browse"
-                  style={{
-                    color: pathname.includes("/browse") ? `${activeColor}` : "",
-                    fontWeight: pathname.includes("/browse") ? "bold" : "",
-                  }}
-                  onClick={() => setIsMenuOpen(false)}
-                  className="text-sm text-[#000] lg:text-[#FFFFFFA0] max-lg:w-full max-lg:h-full max-lg:hover:bg-[#48343422] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                    href="/"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                  Browse
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        AcadVault2.0
+                    </span>
                 </a>
-                <a
-                  href="/new-material"
-                  style={{
-                    color: pathname === "/new-material" ? `${activeColor}` : "",
-                    fontWeight: pathname === "/new-material" ? "bold" : "",
-                  }}
-                  onClick={() => setIsMenuOpen}
-                  className="text-sm text-[#000] lg:text-[#FFFFFFA0] w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
-                >
-                  Upload
-                </a>
-                {isResourceManager && (
-                  <a
-                    href="/requests"
-                    style={{
-                      color: pathname === "/requests" ? `${activeColor}` : "",
-                      fontWeight: pathname === "/requests" ? "bold" : "",
-                    }}
-                    onClick={() => setIsMenuOpen}
-                    className="text-sm text-[#000] lg:text-[#FFFFFFA0] w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
-                  >
-                    Requests
-                  </a>
-                )}
-                <a
-                  href="/about"
-                  style={{
-                    color: pathname === "/about" ? `${activeColor}` : "",
-                    fontWeight: pathname === "/about" ? "bold" : "",
-                  }}
-                  onClick={() => setIsMenuOpen}
-                  className="text-sm text-[#000] lg:text-[#FFFFFFA0] w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
-                >
-                  About
-                </a>
-                <a
-                  href="/me"
-                  style={{
-                    color: pathname === "/me" ? `${activeColor}` : "",
-                    fontWeight: pathname === "/me" ? "bold" : "",
-                  }}
-                  onClick={() => setIsMenuOpen}
-                  className="text-sm text-[#000] lg:text-[#FFFFFFA0] w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
-                >
-                  {isMenuOpen ? (
-                    "Profile"
-                  ) : (
-                    <FaRegUserCircle className="inline-block w-5 h-5 me-1" />
-                  )}
-                </a>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+                <div className={'flex flex-col rtl:space-x-reverse'}>
+                    <button
+                        className="text-white focus:outline-none lg:hidden"
+                        onClick={toggleMenu}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            {isMenuOpen ? (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            ) : (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            )}
+                        </svg>
+                    </button>
+                    <div
+                        className={`${
+                            isMenuOpen
+                                ? 'flex absolute right-5 top-16 py-3 z-50 bg-white bg-opacity-[0.02] backdrop backdrop-blur-sm border border-gray-500 rounded-md  min-w-[200px] items-center text-white'
+                                : 'hidden'
+                        } flex-col lg:flex lg:flex-row lg:items-center lg:w-auto lg:space-x-6 rtl:space-x-reverse`}
+                    >
+                        {session ? (
+                            <>
+                                <a
+                                    href="/browse"
+                                    style={{
+                                        color: pathname.includes('/browse')
+                                            ? `${activeColor}`
+                                            : '',
+                                        fontWeight: pathname.includes('/browse')
+                                            ? 'bold'
+                                            : '',
+                                    }}
+                                    onClick={() => setIsMenuOpen(false)}
+                                    className="text-sm text-white h max-lg:w-full max-lg:h-full max-lg:hover:bg-[#48343422] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                >
+                                    Browse
+                                </a>
+                                <a
+                                    href="/new-material"
+                                    style={{
+                                        color:
+                                            pathname === '/new-material'
+                                                ? `${activeColor}`
+                                                : '',
+                                        fontWeight:
+                                            pathname === '/new-material'
+                                                ? 'bold'
+                                                : '',
+                                    }}
+                                    onClick={() => setIsMenuOpen}
+                                    className="text-sm text-white w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                >
+                                    Upload
+                                </a>
+                                {isResourceManager && (
+                                    <a
+                                        href="/requests"
+                                        style={{
+                                            color:
+                                                pathname === '/requests'
+                                                    ? `${activeColor}`
+                                                    : '',
+                                            fontWeight:
+                                                pathname === '/requests'
+                                                    ? 'bold'
+                                                    : '',
+                                        }}
+                                        onClick={() => setIsMenuOpen}
+                                        className="text-sm text-white w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                    >
+                                        Requests
+                                    </a>
+                                )}
+                                <a
+                                    href="/about"
+                                    style={{
+                                        color:
+                                            pathname === '/about'
+                                                ? `${activeColor}`
+                                                : '',
+                                        fontWeight:
+                                            pathname === '/about' ? 'bold' : '',
+                                    }}
+                                    onClick={() => setIsMenuOpen}
+                                    className="text-sm text-white w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                >
+                                    About
+                                </a>
+                                <a
+                                    href="/me"
+                                    style={{
+                                        color:
+                                            pathname === '/me'
+                                                ? `${activeColor}`
+                                                : '',
+                                        fontWeight:
+                                            pathname === '/me' ? 'bold' : '',
+                                    }}
+                                    onClick={() => setIsMenuOpen}
+                                    className="text-sm text-white w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                >
+                                    {isMenuOpen ? (
+                                        'Profile'
+                                    ) : (
+                                        <FaRegUserCircle className="inline-block w-5 h-5 me-1" />
+                                    )}
+                                </a>
+                            </>
+                        ) : (
+                            <>
+                                <a
+                                    href="/about"
+                                    style={{
+                                        color:
+                                            pathname === '/about'
+                                                ? `${activeColor}`
+                                                : '',
+                                        fontWeight:
+                                            pathname === '/about' ? 'bold' : '',
+                                    }}
+                                    onClick={() => setIsMenuOpen}
+                                    className="text-sm text-white w-full h-full hover:bg-[#0002] lg:hover:text-blue-400 transition-all max-lg:px-5 max-lg:py-2 lg:mt-0"
+                                >
+                                    About
+                                </a>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
-export default NavBar;
+export default NavBar
