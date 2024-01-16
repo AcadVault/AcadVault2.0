44e26e56794ae712c11d7069d21d2217bbbd855b
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-16 23:09:29 +0530

    Add disconnectMongoDB function call in GET route

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import { User } from '@/models/user.model';
-import { connectMongoDB } from "@/lib/mongodb.config";
+import { connectMongoDB, disconnectMongoDB } from "@/lib/mongodb.config";
 
 export const GET = async () => {
   try {
+    await disconnectMongoDB();
     await connectMongoDB();
     const users = await User.find().count();
     return NextResponse.json({ success: true, data: users});
