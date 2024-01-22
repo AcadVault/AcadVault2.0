5aba610ebf0572ab43f2bf0a8d98a53d54f64856
Pranshu Patel <pranshu05patel@gmail.com>
2024-01-22 20:09:01 +0530

    Refactor data fetching in RequestsPage component

--- src/app/requests/page.jsx
+++ src/app/requests/page.jsx
@@ -6,29 +6,29 @@ import Loading from "@/components/Loading";
 import NothingHere from "@/components/NothingHere";
 
 function RequestsPage() {
-  const [data, setData] = useState(null);
+  const [cardData, setCardData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/requests");
-      const data = await response.json();
-      if(data.success) setData(data.data);
-      else throw new Error(data.error);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
   useEffect(() => {
-    fetchData();
+    const fetchCardData = async () => {
+      try {
+        const response = await fetch("/api/requests");
+        const dataJson = await response.json();
+        if (dataJson.success) setCardData(dataJson.data);
+        else throw new Error(dataJson.error);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchCardData();
   }, []);
 
-  if (data === null) return <Loading />;
-  if (data.length === 0) return <NothingHere />;
+  if (cardData === null) return <Loading />;
+  if (cardData.length === 0) return <NothingHere />;
 
   return (
     <div className="w-11/12 sm:w-4/5 md:w-[78%] mx-auto my-10">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-        {data.map((request, index) => (
+        {cardData.map((request, index) => (
           <RequestCard data={request} key={index} />
         ))}
       </div>
