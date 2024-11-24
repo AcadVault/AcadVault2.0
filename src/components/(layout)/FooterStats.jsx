bcfbada8bd183162aa78d4998b1d624fcd1d66c6
Pranshu Patel <pranshu05patel@gmail.com>
2024-11-24 15:40:49 +0530

    feat: Remove total materials size display from FooterStats component

--- src/components/(layout)/FooterStats.jsx
+++ src/components/(layout)/FooterStats.jsx
@@ -12,14 +12,11 @@ const FooterStats = () => {
         try {
             const usersResponse = await fetch("/api/users/");
             const materialResponse = await fetch("/api/requests/");
-            const materialSizeResponse = await fetch("/api/materials/totalSize");
             const usersData = await usersResponse.json();
             const materialData = await materialResponse.json();
-            const materialSizeData = await materialSizeResponse.json();
 
             if (usersData.success) setTotalUsers(usersData.data);
             if (materialData.success) setTotalMaterials(materialData.data.filter(item => item.status === "APPROVED").length);
-            if (materialSizeData.success) setTotalMaterialsSize(materialSizeData.totalSize);
 
             setLoading(false);
         } catch (err) {
@@ -36,7 +33,7 @@ const FooterStats = () => {
     }
 
     return (
-        <span>Total Users: {totalUsers} | Total Materials: {totalMaterials} | Total Materials Size: {totalMaterialsSize} </span>
+        <span>Total Users: {totalUsers} | Total Materials: {totalMaterials} </span>
     );
 };
 
