cbc1a77fd399ddd72094a703330df40443ca421a
Pranshu Patel <pranshu05patel@gmail.com>
2025-02-12 20:23:13 +0530

    feat: Sort contributors by upload count in DriveContributors component

--- src/components/(about)/DriveContributors.jsx
+++ src/components/(about)/DriveContributors.jsx
@@ -25,9 +25,11 @@ const DriveContributors = () => {
         return <div className='w-full my-8 text-center'>Loading...</div>;
     }
 
+    const sortedContributors = [...contributors].sort((a, b) => b.uploadCount - a.uploadCount);
+
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mx-auto w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/3 gap-4 p-1 md:p-5 mt-5 mb-10">
-            {contributors.map((contributor, index) => (
+            {sortedContributors.map((contributor, index) => (
                 <a href={`/userContributions/${contributor.email.split('@')[0]}`} key={index} className='p-2 bg-neutral-950 hover:bg-neutral-950 border border-gray-300 rounded-lg text-center flex flex-col gap-1'>
                     <div>{contributor.name}</div>
                     <div className='text-sm text-gray-300'>
